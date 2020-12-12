5a60045a8d3a0ec67238df93fdc519945e6c1557
Cory Hammon <coryhammon1@gmail.com>
2020-12-12 11:56:18 -0700

    Added rollup config

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import babel from "@rollup/plugin-babel";
+import replace from "@rollup/plugin-replace";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+
+const NODE_ENV = process.env.NODE_ENV || "development";
+//const outputFile = NODE_ENV === "production" ? "./lib/prod.js" : "./lib/dev.js";
+
+export default {
+    input: 'src/app.js',
+    output: {
+        file: 'dist/app.js'
+    },
+    plugins: [
+        replace({
+            "process.env.NODE_ENV": JSON.stringify(NODE_ENV)
+        }),
+        resolve(),
+        babel({ babelHelpers: "bundled", exclude: "node_modules/**" }),
+        commonjs()
+    ]
+}
