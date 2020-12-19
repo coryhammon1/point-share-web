0a5f658c5187cbff76fde87752be118a8b62721a
Cory Hammon <coryhammon1@gmail.com>
2020-12-19 10:46:01 -0700

    Added snowpack for development and bundling

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,37 @@
-import babel from "@rollup/plugin-babel";
-import replace from "@rollup/plugin-replace";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import sucrase from "@rollup/plugin-sucrase";
+// import babel from "@rollup/plugin-babel";
+// import replace from "@rollup/plugin-replace";
+// import resolve from "@rollup/plugin-node-resolve";
+// import commonjs from "@rollup/plugin-commonjs";
+// import sucrase from "@rollup/plugin-sucrase";
 
-const NODE_ENV = process.env.NODE_ENV || "development";
-//const outputFile = NODE_ENV === "production" ? "./lib/prod.js" : "./lib/dev.js";
+// const NODE_ENV = process.env.NODE_ENV || "development";
+// //const outputFile = NODE_ENV === "production" ? "./lib/prod.js" : "./lib/dev.js";
 
-export default {
-    input: 'src/app.js',
-    output: {
-        file: 'dist/app.js',
-        format: 'iife'
-    },
-    plugins: [
-        replace({
-            "process.env.NODE_ENV": JSON.stringify(NODE_ENV),
-            //"firebase.initializeApp": "firebase.default.initializeApp"
-        }),
-        // async(),
-        // babel({ 
-        //     babelHelpers: "bundled"
-        // }),
-        resolve({
-            extensions: [".js"]
-        }),
-        sucrase({
-            exclude: ["node_modules/**"],
-            transforms: ["jsx"]
-        }),
-        commonjs({
-            include: "node_modules/**",
-            sourceMap: true
-        })
-    ]
-}
+// export default {
+//     input: 'src/app.js',
+//     output: {
+//         file: 'dist/app.js',
+//         format: 'iife'
+//     },
+//     plugins: [
+//         replace({
+//             "process.env.NODE_ENV": JSON.stringify(NODE_ENV),
+//             //"firebase.initializeApp": "firebase.default.initializeApp"
+//         }),
+//         // async(),
+//         // babel({ 
+//         //     babelHelpers: "bundled"
+//         // }),
+//         resolve({
+//             extensions: [".js"]
+//         }),
+//         sucrase({
+//             exclude: ["node_modules/**"],
+//             transforms: ["jsx"]
+//         }),
+//         commonjs({
+//             include: "node_modules/**",
+//             sourceMap: true
+//         })
+//     ]
+// }
--- src/components/transactionform.jsx
+++ src/components/transactionform.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function(props) {
-    return <div>Transaction Form</div>;
+    return <div>Transaction Form!</div>;
 }
